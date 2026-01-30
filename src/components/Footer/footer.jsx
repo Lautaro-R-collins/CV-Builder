cf4979f0ffe4afc928a3cc45bfcaca66ad15b606
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-29 23:27:51 -0300

    refactor footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,15 +1,22 @@
-import { Github } from 'lucide-react';
-   
-   
+import { Github } from "lucide-react";
+
 const Footer = () => {
-    return (
-        <footer className="mt-auto">
-            <p className="text-center text-gray-500 text-md mt-5">Developed by Lautaro Rodriguez Collins</p>
-            <a href="https://github.com/lrcollins" target="_blank" rel="noopener noreferrer">
-                <Github size={20} />
-            </a>
-        </footer>
-    );
+  return (
+    <footer className="mt-auto flex justify-center items-center gap-2 py-5">
+      <p className="text-gray-500 text-xl">
+        Developed by Lautaro Rodriguez Collins
+      </p>
+
+      <a
+        href="https://github.com/lrcollins"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-gray-500 hover:text-gray-700 transition-colors"
+      >
+        <Github size={20} />
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
